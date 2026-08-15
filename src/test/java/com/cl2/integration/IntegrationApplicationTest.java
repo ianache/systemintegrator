1eb882899e3a68d17a768dad1d1127f73c80a1af
ianache <ianache@comsatel.com.pe>
2026-08-14 21:26:17 -0500

    build: bootstrap integration application

---
+++ src/test/java/com/cl2/integration/IntegrationApplicationTest.java
@@ -0,0 +1,12 @@
+package com.cl2.integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegrationApplicationTest {
+
+    @Test
+    void applicationContextLoadsWithoutExternalQaServices() {
+    }
+}
